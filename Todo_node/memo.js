5f1f1a718d91b68ac29d2ca0662097542293c94f
Daen12 <sdy971213@gmail.com>
2022-12-02 11:28:23 +0900

    step 2 in progress

--- Todo_node/memo.js
+++ Todo_node/memo.js
@@ -1,2 +1,10 @@
-const direction = "L";
-console.log(direction === "R" || direction === "L");
+const form = [
+    ["R", "R", "W"],
+    ["G", "C", "W"],
+    ["G", "B", "B"],
+];
+const sequence = form[2];
+sequence.push(sequence.shift());
+
+// [form[0][2], form[1][2], form[2][2]] = [form[1][2], form[2][2], form[0][2]];
+console.log(form);
